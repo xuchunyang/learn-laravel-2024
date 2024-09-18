1527b3d943d94564edb78cb9791633461c0bf730
Xu Chunyang <xuchunyang56@gmail.com>
2024-09-18 15:36:02 +0800

    WIP 研究下 Filament 的特色和工作方式

--- app/Filament/Resources/LolChampionResource.php
+++ app/Filament/Resources/LolChampionResource.php
@@ -5,6 +5,16 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\LolChampionResource\Pages;
 use App\Models\LolChampion;
 use Filament\Forms;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Split;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -20,6 +30,22 @@ class LolChampionResource extends Resource
     {
         return $form
             ->schema([
+                FileUpload::make('attachment')
+                    ->disk('public'),
+                Repeater::make('members')
+                    ->schema([
+                        TextInput::make('name')->required(),
+                        Select::make('role')
+                            ->options([
+                                'member' => 'Member',
+                                'administrator' => 'Administrator',
+                                'owner' => 'Owner',
+                            ])
+                            ->required(),
+                    ])
+                    ->cloneable()
+                    ->collapsible()
+                    ->columnSpanFull(),
                 Forms\Components\TextInput::make('uid')
                     ->required()
                     ->maxLength(255),
@@ -41,6 +67,53 @@ class LolChampionResource extends Resource
                     ->relationship('user', 'name')
                     ->searchable()
                     ->preload(),
+                Tabs::make('Tabs')
+                    ->columnSpanFull()
+                    ->tabs([
+                        Tabs\Tab::make('Tab 1')
+                            ->icon('heroicon-m-bell')
+                            ->schema([
+                                Forms\Components\TextInput::make('tab_1_field_1'),
+                            ]),
+                        Tabs\Tab::make('Tab 2')
+                            ->icon('heroicon-m-bell')
+                            ->schema([
+                                Forms\Components\TextInput::make('tab_2_field_1'),
+                            ]),
+                        Tabs\Tab::make('Tab 3')
+                            ->icon('heroicon-m-bell')
+                            ->schema([
+                                Forms\Components\TextInput::make('tab_3_field_1'),
+                            ]),
+                    ]),
+                Fieldset::make('Label')
+                    ->schema([
+                        Forms\Components\RichEditor::make('abilities')->columnSpanFull(),
+                    ]),
+                Split::make([
+                    Section::make([
+                        TextInput::make('title'),
+                        Textarea::make('content'),
+                    ]),
+                    Section::make([
+                        Toggle::make('is_published'),
+                        Toggle::make('is_featured'),
+                    ])->grow(false),
+                ])->from('md'),
+                Section::make('Rate limiting')
+                    ->description('Prevent abuse by limiting the number of requests per period')
+                    ->columns()
+                    ->schema([
+                        Forms\Components\TextInput::make('rate_limit')
+                            ->numeric()
+                            ->step(10),
+                        Forms\Components\Select::make('rate_limit_period')
+                            ->options([
+                                'minute' => 'Minute',
+                                'hour' => 'Hour',
+                                'day' => 'Day',
+                            ]),
+                    ]),
             ]);
     }
 
